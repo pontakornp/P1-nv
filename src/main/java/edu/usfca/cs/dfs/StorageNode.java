95c8b0d02afb9abc2b960937e2f32bfb5ddedeca
Vikas Ramaneti <ramaneti.vikas91@gmail.com>
2019-10-02 14:50:50 -0700

    Added initial class definitions for StorageNode

--- src/main/java/edu/usfca/cs/dfs/StorageNode.java
+++ src/main/java/edu/usfca/cs/dfs/StorageNode.java
@@ -1,9 +1,34 @@
 package edu.usfca.cs.dfs;
 
-import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class StorageNode {
-	private ArrayList<String> activeStorageNodes;
+	public String nodeId;
+	private String nodeAddress;
+	private BloomFilter bloomFilter;
+	private ConcurrentHashMap<String, Chunck> chunckMetaData; 
 	
-
+	public void storeChunk(String fileName, Integer chunkNumber, byte[] chunkData) {
+		
+	}
+	
+	public Integer getChunckCount(String fileName) {
+		return 0;
+	}
+	
+	public String getChunckLocation(String fileName, Integer chunckNumber) {
+		return "";
+	}
+	
+	public byte[] retreiveChunck() {
+		return null;
+	}
+	
+	public synchronized ConcurrentHashMap<String, Chunck> getChunckMetaData() {
+		return this.chunckMetaData;
+	}
+	
+	public void sendHeartBeat() {
+		
+	}
 }
