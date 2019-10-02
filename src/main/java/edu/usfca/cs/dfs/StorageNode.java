e51c2fafdb28e14dff99d9beb1c0962cce3d21b3
pontakornp <ppontakorn@gmail.com>
2019-10-02 14:53:45 -0700

    add skeleton methods

--- src/main/java/edu/usfca/cs/dfs/StorageNode.java
+++ src/main/java/edu/usfca/cs/dfs/StorageNode.java
@@ -4,6 +4,32 @@ import java.util.ArrayList;
 
 public class StorageNode {
 	private ArrayList<String> activeStorageNodes;
-	
 
+	// store chunk
+	public void storeChunk(String fileName, int chunkNum, byte[] chunkData) {
+
+    }
+
+    // get number of chunks
+    public int getChunkNum(String fileName) {
+        return -1;
+    }
+
+    // get chunk location
+    public int getChunkLocation(String fileName, int chunkNum) {
+	    return -1;
+    }
+
+    // retrieve chunk
+    public byte[] retrieveChunk(String fileName, int chunkNum) {
+	    return new byte[0];
+    }
+
+    // list chunks and file names
+    public void listChunksAndFileNames() {
+
+    }
+
+    // send heartbeat to controller
+    // default sending interval: 5 seconds
 }
