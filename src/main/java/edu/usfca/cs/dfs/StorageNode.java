0f11ea6682a66acde2e59194f54a438ff973381c
pontakornp <ppontakorn@gmail.com>
2019-10-08 15:57:20 -0700

    edit storage node

--- src/main/java/edu/usfca/cs/dfs/StorageNode.java
+++ src/main/java/edu/usfca/cs/dfs/StorageNode.java
@@ -1,9 +1,17 @@
 package edu.usfca.cs.dfs;
 
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import edu.usfca.cs.dfs.config.Config;
 
+import javax.xml.bind.DatatypeConverter;
 import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.zip.DataFormatException;
@@ -15,6 +23,19 @@ public class StorageNode {
 	private String nodeAddress;
 	private BloomFilter bloomFilter;
 	private ConcurrentHashMap<String, Chunk> chunkMetaData;
+
+	public StorageNode() {
+		Config config = new Config();
+		String storageDirectoryPath = config.getstorageDirectoryPath();
+		registerNode(storageDirectoryPath);
+	}
+
+	// register node with controller
+	public void registerNode(String storageDirectoryPath) {
+		String nodeId = "1"; // get nodeId from controller
+		this.nodeId = nodeId;
+		this.nodeAddress = storageDirectoryPath + "/" + nodeId;
+	}
 
 	private double Log2(double n) {
 		return Math.log(n) / Math.log(2);
@@ -42,6 +63,24 @@ public class StorageNode {
 		return entropy;
 	}
 
+	private boolean checkSum(String fileName, byte[] chunkData) throws NoSuchAlgorithmException, IOException {
+		String checksum = "5EB63BBBE01EEED093CB22BB8F5ACDC3";
+		byte[] digest = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(Files.readAllBytes(Paths.get(fileName)));
+			digest = md.digest();
+		} catch (NoSuchAlgorithmException | IOException e) {
+			System.out.println("Algorithm or file does not exist.");
+			return false;
+		}
+		String myChecksum = DatatypeConverter.printHexBinary(digest).toUpperCase();
+		if (myChecksum.equals(checksum)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	// To store chunk in a file,
 	// 1. calculate Shannon Entropy of the files.
 	// If their maximum compression is greater than 0.6 (1 - (entropy bits / 8)), then the chunk should be compressed.
@@ -62,9 +101,16 @@ public class StorageNode {
 			}
 		}
 		// store the chunk in a file
+		try {
+			OutputStream outputStream = new FileOutputStream(nodeAddress, true);
+			outputStream.write(data);
+		} catch (IOException e) {
+			System.out.println("There is a problem when writing stream to file.");
+			return false;
+		}
+		// check sum if the file is corrupted or not
 
-		// check sum with the file is corrupted or not
-		return false;
+		return true;
 	}
 
 	// Reference: https://dzone.com/articles/how-compress-and-uncompress
