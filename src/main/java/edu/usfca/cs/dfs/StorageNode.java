09d69241b3478578ff5585b5136d8e6c6d95a327
pontakornp <ppontakorn@gmail.com>
2019-10-08 13:35:28 -0700

    add shanon entroypy function

--- src/main/java/edu/usfca/cs/dfs/StorageNode.java
+++ src/main/java/edu/usfca/cs/dfs/StorageNode.java
@@ -1,49 +1,62 @@
 package edu.usfca.cs.dfs;
 
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class StorageNode {
 	public String nodeId;
 	private String nodeAddress;
 	private BloomFilter bloomFilter;
-	private ConcurrentHashMap<String, Chunk> chunckMetaData; 
-	
-	public synchronized Integer getChunckCount(String fileName) {
-		return 0;
+	private ConcurrentHashMap<String, Chunk> chunkMetaData;
+
+	private double Log2(double n) {
+		return Math.log(n) / Math.log(2);
 	}
-	
-	public synchronized String getChunckLocation(String fileName, Integer chunckNumber) {
-		return "";
-	}
-	
-	public synchronized byte[] retreiveChunck() {
-		return null;
+
+	private double calculateShannonEntropy(byte[] chunkData) {
+		HashMap counter = new HashMap();
+		int totalCount = 0;
+		for(int i = 0; i < chunkData.length; i++) {
+			int bit = chunkData[i];
+			if (counter.get(bit) == null) {
+				counter.put(bit, 1);
+			} else {
+				counter.put(bit, (Integer)counter.get(bit) + 1);
+			}
+			totalCount++;
+		}
+		double entropy = 0;
+		for(int i = 0; i < chunkData.length; i++){
+			int bit = chunkData[i];
+			int freq = (Integer)counter.get(bit);
+			double prob = (double) freq / totalCount;
+			entropy -= prob * Log2(prob);
+		}
+		return entropy;
 	}
 
 	// To store chunk in a file,
 	// 1. check Shannon Entropy of the files.
 	// If their maximum compression is greater than 0.6 (1 - (entropy bits / 8)), then the chunk should be compressed.
-	// 2. store the chunk in a file
+	// 2. store the chunk
 	// 3. do check sum if the it is corrupted or not
 	// return true if the chunk is not corrupted, else return false
 	public boolean storeChunk(String fileName, Integer chunkNumber, byte[] chunkData) {
-
-
-		return true;
+		return false;
 	}
+
 
 	// get number of chunks
-	public Integer getChunkCount(String fileName) {
+	public synchronized Integer getChunkCount(String fileName) {
 		return 0;
 	}
-
 	// get chunk location
-	public String getChunkLocation(String fileName, Integer chunkMetaData) {
+	public synchronized String getChunkLocation(String fileName, Integer chunkNumber) {
 		return "";
 	}
 
 	// retrieve chunk from a file
-	public byte[] retrieveChunk(String fileName, Integer chunkNumber) {
+	public synchronized byte[] retrieveChunk(String fileName, Integer chunkNumber) {
 		return null;
 	}
 
@@ -51,7 +64,6 @@ public class StorageNode {
 	public void listChunksAndFileNames() {
 
 	}
-	
 
 	// get chunk meta data
 	public synchronized ConcurrentHashMap<String, Chunk> getChunkMetaData() {
