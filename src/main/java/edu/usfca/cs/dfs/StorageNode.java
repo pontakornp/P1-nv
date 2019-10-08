f5ebaab9426bfe9871d52161ea4500ada734fe4b
pontakornp <ppontakorn@gmail.com>
2019-10-08 14:27:28 -0700

    add compress/decompress methods

--- src/main/java/edu/usfca/cs/dfs/StorageNode.java
+++ src/main/java/edu/usfca/cs/dfs/StorageNode.java
@@ -1,7 +1,14 @@
 package edu.usfca.cs.dfs;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 public class StorageNode {
 	public String nodeId;
@@ -36,19 +43,70 @@ public class StorageNode {
 	}
 
 	// To store chunk in a file,
-	// 1. check Shannon Entropy of the files.
+	// 1. calculate Shannon Entropy of the files.
 	// If their maximum compression is greater than 0.6 (1 - (entropy bits / 8)), then the chunk should be compressed.
 	// 2. store the chunk
 	// 3. do check sum if the it is corrupted or not
 	// return true if the chunk is not corrupted, else return false
 	public boolean storeChunk(String fileName, Integer chunkNumber, byte[] chunkData) {
+		// calculate Shannon Entropy
 		double entropy = calculateShannonEntropy(chunkData);
 		int entropyBits = chunkData.length;
 		double compressionBaseline = 0.6 * (1 - (entropyBits / 8));
+		byte[] data = chunkData;
 		if (entropy > compressionBaseline) {
 			//compress
+			data = compress(chunkData);
+			if (data == null) {
+				return false;
+			}
 		}
+		// store the chunk in a file
+
+		// check sum with the file is corrupted or not
 		return false;
+	}
+
+	// Reference: https://dzone.com/articles/how-compress-and-uncompress
+	public byte[] compress(byte[] data) {
+		byte[] output = null;
+		try {
+			Deflater deflater = new Deflater();
+			deflater.setInput(data);
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+			deflater.finish();
+			byte[] buffer = new byte[1024];
+			while (!deflater.finished()) {
+				int count = deflater.deflate(buffer);
+				outputStream.write(buffer, 0, count);
+			}
+			outputStream.close();
+			output = outputStream.toByteArray();
+		} catch (IOException e) {
+			System.out.println("Fail to compress chunk.");
+		}
+		return output;
+
+	}
+
+	// Reference: https://dzone.com/articles/how-compress-and-uncompress
+	public byte[] decompress(byte[] data) {
+		byte[] output = null;
+		try {
+			Inflater inflater = new Inflater();
+			inflater.setInput(data);
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+			byte[] buffer = new byte[1024];
+			while (!inflater.finished()) {
+				int count = inflater.inflate(buffer);
+				outputStream.write(buffer, 0, count);
+			}
+			outputStream.close();
+			output = outputStream.toByteArray();
+		} catch (IOException | DataFormatException e) {
+			System.out.println("Fail to decompress chunk.");
+		}
+		return output;
 	}
 
 
