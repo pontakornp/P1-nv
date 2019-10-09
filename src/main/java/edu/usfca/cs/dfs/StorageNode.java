551d21474ee16e923c0883079080275374b186cf
pontakornp <ppontakorn@gmail.com>
2019-10-08 21:56:42 -0700

    refactor methods

--- src/main/java/edu/usfca/cs/dfs/StorageNode.java
+++ src/main/java/edu/usfca/cs/dfs/StorageNode.java
@@ -15,6 +15,11 @@ import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
 import edu.usfca.cs.dfs.config.Config;
+import edu.usfca.cs.dfs.util.CheckSum;
+import edu.usfca.cs.dfs.util.CompressDecompress;
+import edu.usfca.cs.dfs.util.Entropy;
+
+import javax.xml.bind.DatatypeConverter;
 
 public class StorageNode {
 	private String storageNodeId;
@@ -27,12 +32,7 @@ public class StorageNode {
 	public String getStorageNodeId() {
 		return this.storageNodeId;
 	}
-	
-	
-	public void registerNode() {
-		
-	}
-	
+
 	public StorageNode() {
 		Config config = new Config();
 		String storageDirectoryPath = config.getstorageDirectoryPath();
@@ -50,50 +50,18 @@ public class StorageNode {
 		this.strorageNodeAddr = storageDirectoryPath + "/" + nodeId;
 	}
 
-	private double Log2(double n) {
-		return Math.log(n) / Math.log(2);
-	}
-
-	private double calculateShannonEntropy(byte[] chunkData) {
-		HashMap counter = new HashMap();
-		int totalCount = 0;
-		for(int i = 0; i < chunkData.length; i++) {
-			int bit = chunkData[i];
-			if (counter.get(bit) == null) {
-				counter.put(bit, 1);
-			} else {
-				counter.put(bit, (Integer)counter.get(bit) + 1);
-			}
-			totalCount++;
-		}
-		double entropy = 0;
-		for(int i = 0; i < chunkData.length; i++){
-			int bit = chunkData[i];
-			int freq = (Integer)counter.get(bit);
-			double prob = (double) freq / totalCount;
-			entropy -= prob * Log2(prob);
-		}
-		return entropy;
-	}
-
-	private boolean checkSum(String fileName, byte[] chunkData) throws NoSuchAlgorithmException, IOException {
-		String checksum = "5EB63BBBE01EEED093CB22BB8F5ACDC3";
-		byte[] digest = null;
-		try {
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			md.update(Files.readAllBytes(Paths.get(fileName)));
-			digest = md.digest();
-		} catch (NoSuchAlgorithmException | IOException e) {
-			System.out.println("Algorithm or file does not exist.");
-			return false;
-		}
-		String myChecksum = DatatypeConverter.printHexBinary(digest).toUpperCase();
-		if (myChecksum.equals(checksum)) {
+	private boolean isFileCorrupted(String fileName, byte[] chunkData, String originalCheckSum) {
+		// store file
+		// call checksum method
+		String currentCheckSum = CheckSum.checkSum(chunkData);
+		// return true if the checksum matches else return false
+		if (currentCheckSum.equals(originalCheckSum)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
+
 	// To store chunk in a file,
 	// 1. calculate Shannon Entropy of the files.
 	// If their maximum compression is greater than 0.6 (1 - (entropy bits / 8)), then the chunk should be compressed.
@@ -102,13 +70,13 @@ public class StorageNode {
 	// return true if the chunk is not corrupted, else return false
 	public boolean storeChunk(String fileName, Integer chunkNumber, byte[] chunkData) {
 		// calculate Shannon Entropy
-		double entropy = calculateShannonEntropy(chunkData);
+		double entropy = Entropy.calculateShannonEntropy(chunkData);
 		int entropyBits = chunkData.length;
 		double compressionBaseline = 0.6 * (1 - (entropyBits / 8));
 		byte[] data = chunkData;
 		if (entropy > compressionBaseline) {
 			//compress
-			data = compress(chunkData);
+			data = CompressDecompress.compress(chunkData);
 			if (data == null) {
 				return false;
 			}
@@ -125,49 +93,6 @@ public class StorageNode {
 
 		return true;
 	}
-
-	// Reference: https://dzone.com/articles/how-compress-and-uncompress
-	public byte[] compress(byte[] data) {
-		byte[] output = null;
-		try {
-			Deflater deflater = new Deflater();
-			deflater.setInput(data);
-			ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-			deflater.finish();
-			byte[] buffer = new byte[1024];
-			while (!deflater.finished()) {
-				int count = deflater.deflate(buffer);
-				outputStream.write(buffer, 0, count);
-			}
-			outputStream.close();
-			output = outputStream.toByteArray();
-		} catch (IOException e) {
-			System.out.println("Fail to compress chunk.");
-		}
-		return output;
-
-	}
-
-	// Reference: https://dzone.com/articles/how-compress-and-uncompress
-	public byte[] decompress(byte[] data) {
-		byte[] output = null;
-		try {
-			Inflater inflater = new Inflater();
-			inflater.setInput(data);
-			ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-			byte[] buffer = new byte[1024];
-			while (!inflater.finished()) {
-				int count = inflater.inflate(buffer);
-				outputStream.write(buffer, 0, count);
-			}
-			outputStream.close();
-			output = outputStream.toByteArray();
-		} catch (IOException | DataFormatException e) {
-			System.out.println("Fail to decompress chunk.");
-		}
-		return output;
-	}
-
 
 	// get number of chunks
 	public synchronized Integer getChunkCount(String fileName) {
