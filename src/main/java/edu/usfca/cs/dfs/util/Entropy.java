ca7ffc48e09bb56874a06e31876d2ccd904c62a1
pontakornp <ppontakorn@gmail.com>
2019-10-08 21:57:20 -0700

    add checksum, compression/decompress/entropy classes in another package

---
+++ src/main/java/edu/usfca/cs/dfs/util/Entropy.java
@@ -0,0 +1,37 @@
+package edu.usfca.cs.dfs.util;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Entropy {
+    public static double calculateShannonEntropy(byte[] input) {
+        if (input.length == 0) {
+            return 0.0;
+        }
+        /* Total up the occurrences of each byte */
+        int[] charCounts = new int[256];
+        for (byte b : input) {
+            charCounts[b & 0xFF]++;
+        }
+        double entropy = 0.0;
+        for (int i = 0; i < 256; ++i) {
+            if (charCounts[i] == 0.0) {
+                continue;
+            }
+            double freq = (double) charCounts[i] / input.length;
+            entropy -= freq * (Math.log(freq) / Math.log(2));
+        }
+        return entropy;
+    }
+    public static void main(String[] args) {
+//        byte[] test = Files.readAllBytes(Paths.get(args[0]));
+        byte[] test = {5,10,5};
+        double entr = calculateShannonEntropy(test);
+        System.out.println("Entropy (bits per byte): "
+                + String.format("%.2f", entr));
+        System.out.println("Optimal Size Reduction: "
+                + String.format("%.0f%%", (1 - (entr / 8)) * 100));
+        System.out.println("Optimal Compression Ratio: "
+                + String.format("%.0f:1", 8 / entr));
+    }
+}
